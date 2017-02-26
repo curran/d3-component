0a16bd53241a380b72216550c75f36e1e5eb1a43
curran <curran.kelleher@gmail.com>
2017-02-26 14:50:27 +0530

    First pass at react-style D3 Components

---
+++ index.js
@@ -0,0 +1,30 @@
+import local from "d3-local";
+export function component(Component){
+  var className = Component.className,
+      tagName = Component.tagName,
+      componentLocal = local();
+
+  return function (context, props){
+    var components = context
+      .selectAll(className ? "." + className : tagName)
+      .data(Array.isArray(props) ? props : [props]);
+    components
+      .exit()
+        .each(function (){
+          var componentInstance = componentLocal.get(this);
+          if(componentInstance.destroy){ componentInstance.destroy(); }
+        })
+        .remove();
+    components
+      .enter()
+      .append(Component.tagName)
+        .attr("class", 
+        .each(function (){
+          componentLocal.set(this, Component());
+        })
+      .merge(components)
+        .each(function (props){
+          d3.select(this).call(componentLocal.get(this), props);
+        });
+  };
+};
