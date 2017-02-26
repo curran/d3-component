23b4df8733dd31415a3053b8f8625144b259894c
curran <curran.kelleher@gmail.com>
2017-02-26 17:45:29 +0530

    Add test for single instance

--- test/component-test.js
+++ test/component-test.js
@@ -7,14 +7,24 @@ var tape = require("tape"),
 
 // An example component.
 function Paragraph(){
-  return function (context, d){ context.text(d); };
+  return function (context, d){
+    context.text(d);
+  };
 }
 Paragraph.tagName = "p";
 
-tape("component()", function(test) {
+tape("A component should render multiple instances.", function(test) {
   var div = d3.select(jsdom.jsdom().body).append("div");
   var paragraph = d3.component(Paragraph);
   div.call(paragraph, ["foo", "bar"]);
   test.equal(div.html(), "<p>foo</p><p>bar</p>");
+  test.end();
+});
+
+tape("A component should render a single instance.", function(test) {
+  var div = d3.select(jsdom.jsdom().body).append("div");
+  var paragraph = d3.component(Paragraph);
+  div.call(paragraph, "foo");
+  test.equal(div.html(), "<p>foo</p>");
   test.end();
 });
