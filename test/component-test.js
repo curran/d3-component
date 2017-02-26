3f9d505cdf0415fd2b5462a45e92243b06cfe058
curran <curran.kelleher@gmail.com>
2017-02-26 17:59:37 +0530

    Add test for nested components

--- test/component-test.js
+++ test/component-test.js
@@ -28,3 +28,30 @@ tape("A component should render a single instance.", function(test) {
   test.equal(div.html(), "<p>foo</p>");
   test.end();
 });
+
+
+function Heading(){
+  return function (context, d){
+    context.text(d);
+  };
+}
+Heading.tagName = "h";
+
+function Post(){
+  var heading = d3.component(Heading);
+  var paragraph = d3.component(Paragraph);
+  return function (context, d){
+    context
+      .call(heading, d.title)
+      .call(paragraph, d.content);
+  };
+}
+Post.tagName = "div";
+
+tape("Nested components.", function(test) {
+  var div = d3.select(jsdom.jsdom().body).append("div");
+  var post = d3.component(Post);
+  div.call(post, { title: "Title", content: "Content here." });
+  test.equal(div.html(), "<div><h>Title</h><p>Content here.</p></div>");
+  test.end();
+});
