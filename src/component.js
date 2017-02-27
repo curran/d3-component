bde8d1c1ec1f94e02eb00b9170693cf4af788948
curran <curran.kelleher@gmail.com>
2017-02-27 15:58:23 +0530

    Rename props back to data (more D3-ish)

--- src/component.js
+++ src/component.js
@@ -1,19 +1,19 @@
-import { select, local } from "d3-selection";
+import { select } from "d3-selection";
 export default function (component){
   var className = component.className,
       tagName = component.tagName,
       render = component.render || function(){};
 
-  return function (selection, props){
+  return function (selection, data){
     var components = selection
       .selectAll(className ? "." + className : tagName)
-      .data(Array.isArray(props) ? props : [props]);
+      .data(Array.isArray(data) ? data : [data]);
     components.exit().remove();
     components
       .enter().append(tagName).attr("class", className)
       .merge(components)
-        .each(function (props){
-          select(this).call(render, props);
+        .each(function (data){
+          select(this).call(render, data);
         });
   };
 };
