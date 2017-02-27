227bb6fd1f2ddec13c93f450fc915bc0b9b9c1bf
curran <curran.kelleher@gmail.com>
2017-02-27 15:04:58 +0530

    Add tests for enter, update, exit

--- test/component-test.js
+++ test/component-test.js
@@ -14,11 +14,13 @@ function Paragraph(){
 Paragraph.tagName = "p";
 var paragraph = d3.component(Paragraph);
 
+
 function Heading(){
   return function (context, d){ context.text(d); };
 }
 Heading.tagName = "h";
 var heading = d3.component(Heading);
+
 
 function Post(){
   return function (context, d){
@@ -30,12 +32,14 @@ function Post(){
 Post.tagName = "div";
 var post = d3.component(Post);
 
+
 /*************************************
  ************ Utilities **************
  *************************************/
 function createDiv(){
   return d3.select(jsdom.jsdom().body).append("div");
 }
+
 
 /*************************************
  ************** Tests ****************
@@ -47,21 +51,25 @@ tape("A component should render a single instance.", function(test) {
   test.end();
 });
 
+
 tape("A component should render multiple instances.", function(test) {
   var div = createDiv();
 
-  div.call(paragraph, [
-    "foo",
-    "bar"
-  ]);
+  // Enter
+  div.call(paragraph, [ "foo", "bar" ]);
+  test.equal(div.html(), "<p>foo</p><p>bar</p>");
 
-  test.equal(div.html(), [
-    "<p>foo</p>",
-    "<p>bar</p>"
-  ].join(""));
+  // Update + Enter
+  div.call(paragraph, [ "fooz", "barz", "baz" ]);
+  test.equal(div.html(), "<p>fooz</p><p>barz</p><p>baz</p>");
+
+  // Exit
+  div.call(paragraph, []);
+  test.equal(div.html(), "");
 
   test.end();
 });
+
 
 tape("Nested components.", function(test) {
   var div = createDiv();
@@ -81,17 +89,21 @@ tape("Nested components.", function(test) {
   test.end();
 });
 
+
 tape("Nested components multiple instances.", function(test) {
   var div = createDiv();
+
   div.call(post, [
     { title: "A", content: "a" },
     { title: "B", content: "b" },
     { title: "C", content: "c" },
   ]);
+
   test.equal(div.html(), [
     "<div><h>A</h><p>a</p></div>",
     "<div><h>B</h><p>b</p></div>",
     "<div><h>C</h><p>c</p></div>"
   ].join(""));
+
   test.end();
 });
