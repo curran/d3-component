f67f57e81865714c208e7fe9bf1dd25daf142522
curran <curran.kelleher@gmail.com>
2017-02-27 14:56:47 +0530

    Clean up tests

--- test/component-test.js
+++ test/component-test.js
@@ -5,41 +5,22 @@ var tape = require("tape"),
       require("d3-selection")
     );
 
-// An example component.
+/*************************************
+ ************ Components *************
+ *************************************/
 function Paragraph(){
-  return function (context, d){
-    context.text(d);
-  };
+  return function (context, d){ context.text(d); };
 }
 Paragraph.tagName = "p";
-
-tape("A component should render multiple instances.", function(test) {
-  var div = d3.select(jsdom.jsdom().body).append("div");
-  var paragraph = d3.component(Paragraph);
-  div.call(paragraph, ["foo", "bar"]);
-  test.equal(div.html(), "<p>foo</p><p>bar</p>");
-  test.end();
-});
-
-tape("A component should render a single instance.", function(test) {
-  var div = d3.select(jsdom.jsdom().body).append("div");
-  var paragraph = d3.component(Paragraph);
-  div.call(paragraph, "foo");
-  test.equal(div.html(), "<p>foo</p>");
-  test.end();
-});
-
+var paragraph = d3.component(Paragraph);
 
 function Heading(){
-  return function (context, d){
-    context.text(d);
-  };
+  return function (context, d){ context.text(d); };
 }
 Heading.tagName = "h";
+var heading = d3.component(Heading);
 
 function Post(){
-  var heading = d3.component(Heading);
-  var paragraph = d3.component(Paragraph);
   return function (context, d){
     context
       .call(heading, d.title)
@@ -47,23 +28,70 @@ function Post(){
   };
 }
 Post.tagName = "div";
+var post = d3.component(Post);
+
+/*************************************
+ ************ Utilities **************
+ *************************************/
+function createDiv(){
+  return d3.select(jsdom.jsdom().body).append("div");
+}
+
+/*************************************
+ ************** Tests ****************
+ *************************************/
+tape("A component should render a single instance.", function(test) {
+  var div = createDiv();
+  div.call(paragraph, "foo");
+  test.equal(div.html(), "<p>foo</p>");
+  test.end();
+});
+
+tape("A component should render multiple instances.", function(test) {
+  var div = createDiv();
+
+  div.call(paragraph, [
+    "foo",
+    "bar"
+  ]);
+
+  test.equal(div.html(), [
+    "<p>foo</p>",
+    "<p>bar</p>"
+  ].join(""));
+
+  test.end();
+});
 
 tape("Nested components.", function(test) {
-  var div = d3.select(jsdom.jsdom().body).append("div");
-  var post = d3.component(Post);
-  div.call(post, { title: "Title", content: "Content here." });
-  test.equal(div.html(), "<div><h>Title</h><p>Content here.</p></div>");
+  var div = createDiv();
+
+  div.call(post, {
+    title: "Title",
+    content: "Content here."
+  });
+
+  test.equal(div.html(), [
+    "<div>",
+      "<h>Title</h>",
+      "<p>Content here.</p>",
+    "</div>"
+  ].join(""));
+
   test.end();
 });
 
 tape("Nested components multiple instances.", function(test) {
-  var div = d3.select(jsdom.jsdom().body).append("div");
-  var post = d3.component(Post);
+  var div = createDiv();
   div.call(post, [
     { title: "A", content: "a" },
     { title: "B", content: "b" },
     { title: "C", content: "c" },
   ]);
-  test.equal(div.html(), "<div><h>A</h><p>a</p></div><div><h>B</h><p>b</p></div><div><h>C</h><p>c</p></div>");
+  test.equal(div.html(), [
+    "<div><h>A</h><p>a</p></div>",
+    "<div><h>B</h><p>b</p></div>",
+    "<div><h>C</h><p>c</p></div>"
+  ].join(""));
   test.end();
 });
