e277d373747c36972c3bb590c6d12b2d5e9305c2
curran <curran.kelleher@gmail.com>
2017-03-01 02:37:18 +0530

    Memoize local.render

--- src/component.js
+++ src/component.js
@@ -1,11 +1,12 @@
 import { select, local } from "d3-selection";
 
-var componentLocal = local();
+var componentLocal = local(),
+    noop = function (){};
 
 export default function (tagName, className){
   var create,
-      render = function (){},
-      destroy = function (){},
+      render = noop,
+      destroy = noop,
       selector = className ? "." + className : tagName;
 
   function component(selection, props){
@@ -15,14 +16,16 @@ export default function (tagName, className){
         enter = update.enter().append(tagName).attr("class", className);
 
     enter.each(function (){
-      componentLocal.set(this, { selection: select(this) });
+      componentLocal.set(this, {
+        selection: select(this)
+      });
     });
 
     if(create){
       enter.each(function (){
         var local = componentLocal.get(this);
         local.state = {};
-        local.render = function (){};
+        local.render = noop;
         create(function setState(state){
           Object.assign(local.state, state);
           local.render();
@@ -30,9 +33,13 @@ export default function (tagName, className){
       });
       enter.merge(update).each(function (props){
         var local = componentLocal.get(this);
-        (local.render = function (){
-          render(local.selection, props, local.state);
-        })();
+        if(local.render === noop){
+          local.render = function (){
+            render(local.selection, local.props, local.state);
+          };
+        }
+        local.props = props;
+        local.render();
       });
       exit.each(function (){
         destroy(componentLocal.get(this).state);
@@ -44,6 +51,7 @@ export default function (tagName, className){
     }
     exit.remove();
   }
+
   component.render = function(_) { return (render = _, component); };
   component.create = function(_) { return (create = _, component); };
   component.destroy = function(_) { return (destroy = _, component); };
