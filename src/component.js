ba1b36fc19ee416baf0597c023674819bc976811
curran <curran.kelleher@gmail.com>
2017-02-28 15:12:38 +0530

    Minor refactoring

--- src/component.js
+++ src/component.js
@@ -1,36 +1,33 @@
 import { select, local } from "d3-selection";
 
-var noop = function (){},
-    stateLocal = local(),
-    renderLocal = local();
-
-function setState(newState){
-  var oldState = stateLocal.get(this);
-  stateLocal.set(this, Object.assign({}, oldState, newState));
-  renderLocal.get(this)();
-}
+var stateLocal = local(),
+    renderLocal = local(),
+    noop = function (){};
 
 export default function (tagName, className){
-  var render = noop,
-      create,
-      destroy,
+  var render = noop, create, destroy,
       selector = className ? "." + className : tagName;
 
   function component(selection, props){
     var data = Array.isArray(props) ? props : [props],
         update = selection.selectAll(selector).data(data),
         exit = update.exit(),
-        enter = update.enter().append(tagName),
+        enter = update.enter().append(tagName).attr("class", className),
         all = enter.merge(update);
-
-    if(className){
-      enter.attr("class", className);
-    }
 
     if(create){
       enter.each(function (){
         renderLocal.set(this, noop);
-        create(setState.bind(this));
+        create(function (state){
+          stateLocal.set(this, Object.assign({}, stateLocal.get(this), state));
+          renderLocal.get(this)();
+        }.bind(this));
+      });
+      all.each(function (props){
+        renderLocal.set(this, function (){
+          select(this).call(render, props, stateLocal.get(this));
+        }.bind(this));
+        renderLocal.get(this)();
       });
       if(destroy){
         exit.each(function (){
@@ -38,22 +35,13 @@ export default function (tagName, className){
           destroy(stateLocal.get(this));
         });
       }
-      all.each(function (props){
-        var rerender = function (){
-          select(this).call(render, props, stateLocal.get(this));
-        }.bind(this);
-        renderLocal.set(this, rerender);
-        rerender();
-      });
     } else {
       all.each(function (props){
         select(this).call(render, props);
       });
     }
-
     exit.remove();
   }
-
   component.render = function(_) { return (render = _, component); };
   component.create = function(_) { return (create = _, component); };
   component.destroy = function(_) { return (destroy = _, component); };
