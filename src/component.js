4ebdfb68a94749ab6b9324e221809bc804221af4
curran <curran.kelleher@gmail.com>
2017-03-01 08:03:18 +0530

    Factor out function creation from render pass

--- src/component.js
+++ src/component.js
@@ -9,40 +9,43 @@ export default function (tagName, className){
       destroy = noop,
       selector = className ? "." + className : tagName;
 
-  function component(selection, props){
-    var components = selection
-      .selectAll(selector)
-      .data(Array.isArray(props) ? props : [props]);
+  function myCreate(){
+    var my = myLocal.set(this, {
+      selection: select(this),
+      state: {},
+      render: noop
+    });
+    create(function setState(state){
+      Object.assign(my.state, state);
+      my.render();
+    });
+    my.render = function (){
+      render(my.selection, my.props, my.state);
+    };
+  }
 
+  function myRender(props){
+    var my = myLocal.get(this);
+    my.props = props;
+    my.render();
+  }
+
+  function myDestroy(props){
+    destroy(myLocal.get(this).state);
+  }
+
+  function component(selection, props){
+    var components = selection.selectAll(selector)
+      .data(Array.isArray(props) ? props : [props]);
     components
-      .enter()
-      .append(tagName)
+      .enter().append(tagName)
         .attr("class", className)
-        .each(function (){
-          var my = myLocal.set(this, {
-            selection: select(this),
-            state: {},
-            render: noop
-          });
-          create(function setState(state){
-            Object.assign(my.state, state);
-            my.render();
-          });
-          my.render = function (){
-            render(my.selection, my.props, my.state);
-          };
-        })
+        .each(myCreate)
       .merge(components)
-        .each(function (props){
-          var my = myLocal.get(this);
-          my.props = props;
-          my.render();
-        });
+        .each(myRender);
     components
       .exit()
-        .each(function (){
-          destroy(myLocal.get(this).state);
-        })
+        .each(myDestroy)
         .remove();
   }
 
