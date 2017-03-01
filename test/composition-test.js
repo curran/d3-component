a5349bfb1b02424086a16d5370597560778d3f28
curran <curran.kelleher@gmail.com>
2017-03-01 11:40:55 +0530

    Move heterogeneous rendering test into composition-test

--- test/composition-test.js
+++ test/composition-test.js
@@ -30,6 +30,17 @@ var card = d3.component("div", "card")
         .call(props.childComponent, props.childProps);
   });
 
+// Conditional rendering
+var apple = d3.component("span", "apple")
+    orange = d3.component("span", "orange")
+    fruit = d3.component("div", "fruit")
+      .render(function (selection, props){
+        selection
+          .call(apple, props.type === "apple"? {} : [])
+          .call(orange, props.type === "orange"? {} : [])
+      });
+
+
 /*************************************
  ************** Tests ****************
  *************************************/
@@ -50,7 +61,6 @@ tape("Nesting.", function(test) {
 
   test.end();
 });
-
 
 tape("Nesting multiple instances.", function(test) {
   var div = d3.select(jsdom.jsdom().body).append("div");
@@ -84,7 +94,6 @@ tape("Nesting multiple instances.", function(test) {
   test.end();
 });
 
-
 tape("Containment.", function(test) {
   var div = d3.select(jsdom.jsdom().body).append("div");
   div.call(card, {
@@ -108,3 +117,36 @@ tape("Containment.", function(test) {
   test.end();
 });
 
+tape("Conditional rendering.", function(test) {
+  var div = d3.select(jsdom.jsdom().body).append("div");
+
+  // Enter
+  div.call(fruit, [
+    { type: "apple" },
+    { type: "orange" },
+    { type: "apple" },
+    { type: "apple" },
+    { type: "orange" }
+  ]);
+  test.equal(div.html(), [
+    '<div class="fruit"><span class="apple"></span></div>',
+    '<div class="fruit"><span class="orange"></span></div>',
+    '<div class="fruit"><span class="apple"></span></div>',
+    '<div class="fruit"><span class="apple"></span></div>',
+    '<div class="fruit"><span class="orange"></span></div>'
+  ].join(""));
+
+  // Update + Exit
+  div.call(fruit, [
+    { type: "orange" },
+    { type: "apple" },
+    { type: "apple" }
+  ]);
+  test.equal(div.html(), [
+    '<div class="fruit"><span class="orange"></span></div>',
+    '<div class="fruit"><span class="apple"></span></div>',
+    '<div class="fruit"><span class="apple"></span></div>'
+  ].join(""));
+
+  test.end();
+});
