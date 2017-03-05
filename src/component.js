9314b744fd99b2da6a1f2528b6abb9b0b238e258
curran <curran.kelleher@gmail.com>
2017-03-06 01:38:28 +0530

    Overhaul API, remove local state feature. Closes #40

--- src/component.js
+++ src/component.js
@@ -1,56 +1,55 @@
 import { select, local } from "d3-selection";
-var noop = function (){}, // no operation
-    children = function (){ return this.children; },
-    instanceLocal = local(),
-    renderInstance = function (props){
-      var instance = instanceLocal.get(this);
-      instance.props = props || {};
-      instance.render();
-    };
+var instanceLocal = local();
 
-export default function (tagName, className){
+function noop (){
+} // no operation
+
+function children(){
+  return this.children;
+}
+
+function destroyInstance(d, i){
+  select(this).selectAll("*").each(function (){
+    var instance = instanceLocal.get(this);
+    instanceLocal.remove(this) && instance.destroy();
+  });
+  (instanceLocal.get(this).destroy(d, i) || select(this)).remove();
+}
+
+export default function (tagName){
   var create = noop,
       render = noop,
       destroy = noop,
-      key,
-      createInstance = function (){
-        var instance = instanceLocal.set(this, {
-          selection: select(this),
-          owner: component,
-          destroy: function (){
-            return destroy(instance.selection, instance.state);
-          }
-        });
-        create(instance.selection, function setState(state){
-          state = (typeof state === "function") ? state(instance.state) : state;
-          instance.state = Object.assign({}, instance.state, state);
-          instance.render && instance.render();
-        });
-        instance.render = function (){
-          render(instance.selection, instance.props, instance.state);
-        };
-      },
-      destroyInstance = function (){
-        var instance = instanceLocal.get(this);
-        instance.selection.selectAll("*").each(function (){
-          var instance = instanceLocal.get(this);
-          instanceLocal.remove(this) && instance.destroy();
-        });
-        (instance.destroy() || instance.selection).remove();
-      },
-      belongsToMe = function (){
-        var instance = instanceLocal.get(this);
-        return instance && instance.owner === component;
-      };
+      key;
 
-  function component(selection, props){
-    var instances = selection.selectAll(children).filter(belongsToMe)
-      .data(Array.isArray(props) ? props : [props], key);
-    instances.enter().append(tagName).attr("class", className)
-        .each(createInstance)
+  function setInstance(d, i){
+    instanceLocal.set(this, {
+      owner: component,
+      destroy: function (){
+        return destroy.call(this, d, i);
+      }.bind(this)
+    });
+  }
+
+  function belongsToMe(){
+    var instance = instanceLocal.get(this);
+    return instance && instance.owner === component;
+  }
+
+  function component(selection, data){
+    var instances = selection
+      .selectAll(children)
+      .filter(belongsToMe)
+      .data(Array.isArray(data) ? data : [data], key);
+    instances
+      .enter().append(tagName)
+        .each(setInstance)
+        .each(create)
       .merge(instances)
-        .each(renderInstance);
-    instances.exit().each(destroyInstance);
+        .each(render);
+    instances
+      .exit()
+        .each(destroyInstance);
   }
   component.render = function(_) { return (render = _, component); };
   component.create = function(_) { return (create = _, component); };
