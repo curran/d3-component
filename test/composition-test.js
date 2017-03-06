b2a6bf0f9b525a71fcc5588e59d7530a765230fb
curran <curran.kelleher@gmail.com>
2017-03-06 17:02:55 +0530

    Reinstate recursive exit test

--- test/composition-test.js
+++ test/composition-test.js
@@ -3,88 +3,15 @@ var tape = require("tape"),
     d3 = Object.assign(require("../"), require("d3-selection")),
     post = require("./nesting-test").post;
 
-//// Recursive destruction
-//var leafDestroyed = 0,
-//    leaf = d3.component("div", "leaf")
-//      .exit(function (){
-//        leafDestroyed ++;
-//      })
-//    twig = d3.component("div", "twig")
-//      .update(function (d3.select(this)){
-//        d3.select(this).call(leaf);
-//      });
-//    branch = d3.component("div", "branch")
-//      .update(function (d3.select(this)){
-//        d3.select(this).call(twig, [1, 2]);
-//      });
-//    treeDestroyed = 0,
-//    tree = d3.component("div", "tree")
-//      .enter(function (d3.select(this)){
-//        d3.select(this)
-//          .append("div")
-//            .attr("class", "trunk");
-//      })
-//      .update(function (d3.select(this)){
-//        d3.select(this)
-//          .select(".trunk")
-//            .call(branch, [1, 2, 3]);
-//      })
-//      .exit(function (){
-//        treeDestroyed ++;
-//      });
-//
-//// Recursive updateing.
+//// Recursive rendering.
 //var recursiveComponent = d3.component("div")
-//  .update(function (d3.select(this), props){
+//  .update(function (props){
 //    d3.select(this)
 //        .attr("class", props.class)
 //        .call(recursiveComponent, props.children || []);
 //  });
-//
-//
-//tape("Recursive destruction.", function(test) {
-//  var div = d3.select(jsdom.jsdom().body).append("div");
-//
-//  div.call(tree);
-//  test.equal(div.html(), [
-//    '<div class="tree">',
-//      '<div class="trunk">',
-//        '<div class="branch">',
-//          '<div class="twig">',
-//            '<div class="leaf"></div>',
-//          '</div>',
-//          '<div class="twig">',
-//            '<div class="leaf"></div>',
-//          '</div>',
-//        '</div>',
-//        '<div class="branch">',
-//          '<div class="twig">',
-//            '<div class="leaf"></div>',
-//          '</div>',
-//          '<div class="twig">',
-//            '<div class="leaf"></div>',
-//          '</div>',
-//        '</div>',
-//        '<div class="branch">',
-//          '<div class="twig">',
-//            '<div class="leaf"></div>',
-//          '</div>',
-//          '<div class="twig">',
-//            '<div class="leaf"></div>',
-//          '</div>',
-//        '</div>',
-//      '</div>',
-//    '</div>',
-//  ].join(""));
-//  test.equal(leafDestroyed, 0);
-//
-//  div.call(tree, []);
-//  test.equal(leafDestroyed, 6);
-//  test.equal(treeDestroyed, 1);
-//
-//  test.end();
-//});
-//
+
+
 //tape("Recursive updateing.", function(test) {
 //  var div = d3.select(jsdom.jsdom().body).append("div");
 //
