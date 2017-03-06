f37a88c61a7706231c4acbf7ef88167b25d1af0c
curran <curran.kelleher@gmail.com>
2017-03-06 18:18:29 +0530

    Add tests for index. Closes #44

--- test/basics-test.js
+++ test/basics-test.js
@@ -12,6 +12,19 @@ var paragraphDatum,
         d3.select(this).text(d);
       });
 
+var indexTesterEnterIndex,
+    indexTesterUpdateIndex,
+    indexTesterExitIndex,
+    indexTester = d3.component("div")
+      .enter(function (d, i){
+        indexTesterEnterIndex = i;
+      })
+      .update(function (d, i){
+        indexTesterUpdateIndex = i;
+      })
+      .exit(function (d, i){
+        indexTesterExitIndex = i;
+      });
 
 tape("A component should render a single instance.", function(test) {
   var div = d3.select(jsdom.jsdom().body).append("div");
@@ -47,5 +60,41 @@ tape("A component should be passed undefined as datum when data not specified.",
   div.call(paragraph);
   test.equal(div.html(), '<p></p>');
   test.equal(typeof paragraphDatum, "undefined");
+  test.end();
+});
+
+tape("A component should be passed the index as the second argument in callbacks.", function(test) {
+  var div = d3.select(jsdom.jsdom().body).append("div");
+  
+  indexTesterEnterIndex = indexTesterUpdateIndex = indexTesterExitIndex = null;
+  div.call(indexTester);
+  test.equal(indexTesterEnterIndex, 0)
+  test.equal(indexTesterUpdateIndex, 0)
+  test.equal(indexTesterExitIndex, null);
+
+  indexTesterEnterIndex = indexTesterUpdateIndex = indexTesterExitIndex = null;
+  div.call(indexTester, ["a", "b"]);
+  test.equal(indexTesterEnterIndex, 1)
+  test.equal(indexTesterUpdateIndex, 1)
+  test.equal(indexTesterExitIndex, null);
+
+  indexTesterEnterIndex = indexTesterUpdateIndex = indexTesterExitIndex = null;
+  div.call(indexTester, ["a", "b"]);
+  test.equal(indexTesterEnterIndex, null)
+  test.equal(indexTesterUpdateIndex, 1)
+  test.equal(indexTesterExitIndex, null);
+
+  indexTesterEnterIndex = indexTesterUpdateIndex = indexTesterExitIndex = null;
+  div.call(indexTester, ["a"]);
+  test.equal(indexTesterEnterIndex, null)
+  test.equal(indexTesterUpdateIndex, 0)
+  test.equal(indexTesterExitIndex, 1);
+
+  indexTesterEnterIndex = indexTesterUpdateIndex = indexTesterExitIndex = null;
+  div.call(indexTester, []);
+  test.equal(indexTesterEnterIndex, null)
+  test.equal(indexTesterUpdateIndex, null)
+  test.equal(indexTesterExitIndex, 0);
+
   test.end();
 });
