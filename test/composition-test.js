97a4491987e2cfd763fb7d5c31f0d5b4ac818eac
curran <curran.kelleher@gmail.com>
2017-03-06 15:05:21 +0530

    Reinstate conditional rendering test

--- test/composition-test.js
+++ test/composition-test.js
@@ -2,17 +2,7 @@ var tape = require("tape"),
     jsdom = require("jsdom"),
     d3 = Object.assign(require("../"), require("d3-selection")),
     post = require("./nesting-test").post;
-//
-//// Conditional updateing
-//var apple = d3.component("span", "apple")
-//    orange = d3.component("span", "orange")
-//    fruit = d3.component("div", "fruit")
-//      .update(function (d3.select(this), props){
-//        d3.select(this)
-//          .call(apple, props.type === "apple"? {} : [])
-//          .call(orange, props.type === "orange"? {} : [])
-//      });
-//
+
 //// Recursive destruction
 //var leafDestroyed = 0,
 //    leaf = d3.component("div", "leaf")
@@ -51,43 +41,6 @@ var tape = require("tape"),
 //        .call(recursiveComponent, props.children || []);
 //  });
 //
-///*************************************
-// ************** Tests ****************
-// *************************************/
-//
-//tape("Conditional updateing.", function(test) {
-//  var div = d3.select(jsdom.jsdom().body).append("div");
-//
-//  // Enter
-//  div.call(fruit, [
-//    { type: "apple" },
-//    { type: "orange" },
-//    { type: "apple" },
-//    { type: "apple" },
-//    { type: "orange" }
-//  ]);
-//  test.equal(div.html(), [
-//    '<div class="fruit"><span class="apple"></span></div>',
-//    '<div class="fruit"><span class="orange"></span></div>',
-//    '<div class="fruit"><span class="apple"></span></div>',
-//    '<div class="fruit"><span class="apple"></span></div>',
-//    '<div class="fruit"><span class="orange"></span></div>'
-//  ].join(""));
-//
-//  // Update + Exit
-//  div.call(fruit, [
-//    { type: "orange" },
-//    { type: "apple" },
-//    { type: "apple" }
-//  ]);
-//  test.equal(div.html(), [
-//    '<div class="fruit"><span class="orange"></span></div>',
-//    '<div class="fruit"><span class="apple"></span></div>',
-//    '<div class="fruit"><span class="apple"></span></div>'
-//  ].join(""));
-//
-//  test.end();
-//});
 //
 //tape("Recursive destruction.", function(test) {
 //  var div = d3.select(jsdom.jsdom().body).append("div");
