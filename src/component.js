b66fab88d2982250aabb81ddff882990f0987b5b
curran <curran.kelleher@gmail.com>
2017-03-09 12:18:32 +0530

    Reorganize code

--- src/component.js
+++ src/component.js
@@ -1,49 +1,12 @@
 import { select, local } from "d3-selection";
-var instanceLocal = local();
-
-function noop(){} // no operation
-
-function children(){
-  return this.children;
-}
-
-function exitDescendant(){
-  var instance = instanceLocal.get(this);
-  instanceLocal.remove(this) && instance.exit();
-}
-
-function exitInstance(){
-  select(this).selectAll("*").each(exitDescendant);
-  (instanceLocal.get(this).exit() || select(this)).remove();
-}
-
-function dataArray(data, context){
-  data = Array.isArray(data) ? data : [data];
-  return context ? data.map(function (d){
-    return Object.assign({}, d, context);
-  }) : data;
-}
+var instanceLocal = local(),
+    noop = function (){}; // no operation
 
 export default function (tagName, className){
   var enter = noop,
       update = noop,
       exit = noop,
       key;
-
-  function enterInstance(d, i, nodes){
-    instanceLocal.set(this, {
-      owner: component,
-      exit: function (){
-        return exit.call(this, d, i, nodes);
-      }.bind(this)
-    });
-    enter.call(this, d, i, nodes);
-  }
-
-  function belongsToMe(){
-    var instance = instanceLocal.get(this);
-    return instance && instance.owner === component;
-  }
 
   function component(selection, data, context){
     var instances = selection
@@ -65,6 +28,42 @@ export default function (tagName, className){
   component.enter = function(_) { return (enter = _, component); };
   component.exit = function(_) { return (exit = _, component); };
   component.key = function(_) { return (key = _, component); };
+
+  function children(){
+    return this.children;
+  }
+
+  function belongsToMe(){
+    var instance = instanceLocal.get(this);
+    return instance && instance.owner === component;
+  }
+
+  function dataArray(data, context){
+    data = Array.isArray(data) ? data : [data];
+    return context ? data.map(function (d){
+      return Object.assign({}, d, context);
+    }) : data;
+  }
+
+  function enterInstance(d, i, nodes){
+    instanceLocal.set(this, {
+      owner: component,
+      exit: function (){
+        return exit.call(this, d, i, nodes);
+      }.bind(this)
+    });
+    enter.call(this, d, i, nodes);
+  }
+
+  function exitInstance(){
+    select(this).selectAll("*").each(exitDescendant);
+    (instanceLocal.get(this).exit() || select(this)).remove();
+  }
+
+  function exitDescendant(){
+    var instance = instanceLocal.get(this);
+    instanceLocal.remove(this) && instance.exit();
+  }
 
   return component;
 };
