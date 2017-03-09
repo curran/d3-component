947b996bd06cb214f85c9820db7db78e6973a289
curran <curran.kelleher@gmail.com>
2017-03-09 12:06:47 +0530

    Add more tests for context

--- test/basics-test.js
+++ test/basics-test.js
@@ -5,32 +5,123 @@ var tape = require("tape"),
     d3 = Object.assign(d3_selection, d3_component);
 
 
-var paragraph = d3.component("p")
+var paragraphDatum,
+    paragraph = d3.component("p")
       .update(function (d){
-        d3.select(this).text(d.text);
-        d.callback();
+        paragraphDatum = d;
+        d3.select(this).text(d);
+      });
+
+var enterIndex,
+    updateIndex,
+    exitIndex,
+    enterNodes,
+    updateNodes,
+    exitNodes,
+    argsTester = d3.component("div")
+      .enter(function (d, i, nodes){
+        enterIndex = i;
+        enterNodes = nodes;
+      })
+      .update(function (d, i, nodes){
+        updateIndex = i;
+        updateNodes = nodes;
+      })
+      .exit(function (d, i, nodes){
+        exitIndex = i;
+        exitNodes = nodes;
       });
 
 
-tape("A component accept a context.", function(test) {
+tape("A component should render a single instance.", function(test) {
   var div = d3.select(jsdom.jsdom().body).append("div");
-
-  var data = [
-    { text: "foo" },
-    { text: "bar" }
-  ];
-
-  var count;
-  var context = {
-    callback: function (){
-      count++;
-    }
-  };
-
-  div.call(paragraph, data, context);
-  test.equal(div.html(), "<p>foo</p><p>bar</p>");
-  test.equal(count, 2);
+  div.call(paragraph, "Hello Component");
+  test.equal(div.html(), "<p>Hello Component</p>");
   test.end();
-
 });
 
+tape("A component should render multiple instances.", function(test) {
+  var div = d3.select(jsdom.jsdom().body).append("div");
+
+  // Enter
+  div.call(paragraph, [ "foo", "bar" ]);
+  test.equal(div.html(), "<p>foo</p><p>bar</p>");
+
+  // Update + Enter
+  div.call(paragraph, [ "fooz", "barz", "baz" ])
+  test.equal(div.html(), "<p>fooz</p><p>barz</p><p>baz</p>");
+
+  // Update + Exit
+  div.call(paragraph, [ "fooz", "baz" ])
+  test.equal(div.html(), "<p>fooz</p><p>baz</p>");
+
+  // Exit
+  div.call(paragraph, []);
+  test.equal(div.html(), "");
+
+  test.end();
+});
+
+tape("A component should be passed undefined as datum when data not specified.", function(test) {
+  var div = d3.select(jsdom.jsdom().body).append("div");
+  div.call(paragraph);
+  test.equal(div.html(), '<p></p>');
+  test.equal(typeof paragraphDatum, "undefined");
+  test.end();
+});
+
+tape("A component should be passed the index and nodes in callbacks.", function(test) {
+  var div = d3.select(jsdom.jsdom().body).append("div");
+  
+  enterIndex = updateIndex = exitIndex = null;
+  enterNodes = updateNodes = exitNodes = null;
+  div.call(argsTester);
+  test.equal(enterIndex, 0)
+  test.equal(updateIndex, 0)
+  test.equal(exitIndex, null);
+  test.equal(enterNodes.length, 1);
+  test.equal(updateNodes.length, 1)
+  test.equal(exitNodes, null);
+
+  enterIndex = updateIndex = exitIndex = null;
+  enterNodes = updateNodes = exitNodes = null;
+  div.call(argsTester, ["a", "b"]);
+  test.equal(enterIndex, 1)
+  test.equal(updateIndex, 1)
+  test.equal(exitIndex, null);
+  test.equal(enterNodes.length, 2);
+  test.equal(updateNodes.length, 2)
+  test.equal(exitNodes, null);
+
+  enterIndex = updateIndex = exitIndex = null;
+  enterNodes = updateNodes = exitNodes = null;
+  div.call(argsTester, ["a", "b"]);
+  test.equal(enterIndex, null)
+  test.equal(updateIndex, 1)
+  test.equal(exitIndex, null);
+  test.equal(enterNodes, null);
+  test.equal(updateNodes.length, 2)
+  test.equal(exitNodes, null);
+
+  enterIndex = updateIndex = exitIndex = null;
+  enterNodes = updateNodes = exitNodes = null;
+  div.call(argsTester, ["a"]);
+  test.equal(enterIndex, null)
+  test.equal(updateIndex, 0)
+  test.equal(exitIndex, 1);
+  test.equal(enterNodes, null);
+  test.equal(updateNodes.length, 1)
+  test.equal(exitNodes.length, 2);
+
+  enterIndex = updateIndex = exitIndex = null;
+  enterNodes = updateNodes = exitNodes = null;
+  div.call(argsTester, []);
+  test.equal(enterIndex, null)
+  test.equal(updateIndex, null)
+  test.equal(exitIndex, 0);
+  test.equal(enterNodes, null);
+  test.equal(updateNodes, null);
+  test.equal(exitNodes.length, 1);
+
+  test.end();
+});
---
+++ test/context-test.js
@@ -0,0 +1,47 @@
+var tape = require("tape"),
+    jsdom = require("jsdom"),
+    d3_selection = require("d3-selection"),
+    d3_component = require("../"),
+    d3 = Object.assign(d3_selection, d3_component);
+
+var paragraph = d3.component("p")
+      .update(function (d){
+        d3.select(this).text(d.text);
+        d.callback();
+      });
+
+tape("A component accept a context.", function(test) {
+  var div = d3.select(jsdom.jsdom().body).append("div");
+
+  var data = [
+    { text: "foo" },
+    { text: "bar" }
+  ];
+
+  var count = 0;
+  var context = {
+    callback: function (){
+      count++;
+    }
+  };
+
+  // Multiple instances
+  div.call(paragraph, data, context);
+  test.equal(div.html(), "<p>foo</p><p>bar</p>");
+  test.equal(count, 2);
+
+  // Single instance
+  div.call(paragraph, data[0], context);
+  test.equal(div.html(), "<p>foo</p>");
+  test.equal(count, 3);
+
+  // Context properties override data properties.
+  context.text = "pwn";
+  div.call(paragraph, data[0], context);
+  test.equal(div.html(), "<p>pwn</p>");
+  test.equal(count, 4);
+
+  test.end();
+
+});
+
