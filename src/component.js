6b1a9acd797d23347ff499a57cd39193e93af365
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-20 03:35:57 -0700

    eslint

--- src/component.js
+++ src/component.js
@@ -1,15 +1,16 @@
-import { select } from "d3-selection";
-const setInstance = (node, value) => { node.__instance__ = value }; // no operation
+import { select } from 'd3-selection';
+
+const setInstance = (node, value) => { node.__instance__ = value; }; // no operation
 const getInstance = node => node.__instance__;
 const noop = () => {};
 
-export default function (tagName, className){
+export default function (tagName, className) {
   let create = noop;
   let render = noop;
   let destroy = noop;
   let key;
 
-  function component(selection, data, context){
+  function component(selection, data, context) {
     const instances = (selection.nodeName ? select(selection) : selection)
       .selectAll(mine)
       .data(dataArray(data, context), key);
@@ -18,48 +19,60 @@ export default function (tagName, className){
         .each(destroyInstance);
     return instances
       .enter().append(tagName)
-        .attr("class", className)
+        .attr('class', className)
         .each(createInstance)
       .merge(instances)
         .each(render);
   }
 
-  function mine(){
+  function mine() {
     return Array.from(this.children).filter(belongsToMe);
   }
 
-  function belongsToMe(node){
+  function belongsToMe(node) {
     const instance = getInstance(node);
     return instance && instance.owner === component;
   }
 
-  function dataArray(data, context){
+  function dataArray(data, context) {
     data = Array.isArray(data) ? data : [data];
     return context ? data.map(d => Object.assign(Object.create(context), d)) : data;
   }
 
-  function createInstance(d, i, nodes){
+  function createInstance(d, i, nodes) {
     setInstance(this, {
       owner: component,
-      destroy: () => destroy.call(this, d, i, nodes)
+      destroy: () => destroy.call(this, d, i, nodes),
     });
     create.call(this, d, i, nodes);
   }
 
-  function destroyInstance(){
-    select(this).selectAll("*").each(destroyDescendant);
+  function destroyInstance() {
+    select(this).selectAll('*').each(destroyDescendant);
     (getInstance(this).destroy() || select(this)).remove();
   }
 
-  function destroyDescendant(){
+  function destroyDescendant() {
     const instance = getInstance(this);
-    instance && instance.destroy();
+    if (instance) { instance.destroy(); }
   }
 
-  component.render = _ => (render = _, component);
-  component.create = _ => (create = _, component);
-  component.destroy = _ => (destroy = _, component);
-  component.key = _ => (key = _, component);
+  component.render = (_) => {
+    render = (_, component);
+    return render;
+  };
+  component.create = (_) => {
+    create = (_, component);
+    return create;
+  };
+  component.destroy = (_) => {
+    destroy = (_, component);
+    return destroy;
+  };
+  component.key = (_) => {
+    key = (_, component);
+    return key;
+  };
 
   return component;
 }
